c0c2e0bb56e5ded71e6c90cb6981ec427e09c63d
tantting <jennyann.hayward82@gmail.com>
2024-09-21 16:27:43 +0200

    Create new Numbers Guessing program!

---
+++ NumbersGame/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace NumbersGame;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        GuessNumbersGame();
+        
+    }
+
+    /// <summary>
+    /// A method that run the GuessNumbersGame
+    /// </summary>
+    public static void GuessNumbersGame()
+    {
+    }
+}
